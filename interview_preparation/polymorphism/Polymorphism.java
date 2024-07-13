5ebe86bac67330a8c3bfffda128b1a3545fc16df
hagos <111482114+Kytsync@users.noreply.github.com>
2024-07-12 20:08:31 -0400

    refactored code

--- interview_preparation/polymorphism/Polymorphism.java
+++ interview_preparation/polymorphism/Polymorphism.java
@@ -4,34 +4,34 @@ public class Polymorphism {
     // Overloading can happen
 
     // over laoding
-      // different variable count
-      // different data types
-
-    int add(int a, int b) {
-        return a+b;
-    }
-
-    // different data type and same count with the above
-    double add(double a, double b) {
-        return a+b;
-    }
-
-    int add(int a, int b, int f, int l) {
-        return a+b;
-    }
-
-    int add(int a, int b, int k) {
-        return a+b;
-    }
+    // different variable count
+    // different data types
 
     public static void main(String[] args) {
         Polymorphism poly = new Polymorphism();
 
-        poly.add(2 ,4); // this is one demonstration of data types.
+        poly.add(2, 4); // this is one demonstration of data types.
         // it can also happen in sub class /  super class hierarichy.
 
-        poly.add(3,4,5); //
+        poly.add(3, 4, 5); //
 
+    }
+
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    // different data type and same count with the above
+    double add(double a, double b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int f, int l) {
+        return a + b;
+    }
+
+    int add(int a, int b, int k) {
+        return a + b;
     }
 
 }
